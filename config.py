5b73697c98638e1701a57c3f21aa0ed82a84b5bb
Suraj S <116282102+suraj0-11@users.noreply.github.com>
2023-07-14 21:10:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 from os import environ
 
 API_ID = 5168062
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_HASH = "04c049aa96d1cc87920b45b7fb43c0d0"
+BOT_TOKEN = "5777523600:AAFKlGOd2rMTpKfoNfNSjhwiqfj3xSDSKcQ"
 ADMIN = 1768419719          
 CAPTION = environ.get("CAPTION", "")
-config.API_HASH = "04c049aa96d1cc87920b45b7fb43c0d0"
-config.BOT_TOKEN = "5777523600:AAFKlGOd2rMTpKfoNfNSjhwiqfj3xSDSKcQ"
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 
