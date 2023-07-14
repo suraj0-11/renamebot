64b9c75cde78a3b23b39a50bdb9b916613f923bb
Suraj S <116282102+suraj0-11@users.noreply.github.com>
2023-07-14 21:01:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
+API_ID = 5168062
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "")
-config.API_ID = 5168062
 config.API_HASH = "04c049aa96d1cc87920b45b7fb43c0d0"
 config.BOT_TOKEN = "5777523600:AAFKlGOd2rMTpKfoNfNSjhwiqfj3xSDSKcQ"
 config.ADMIN = [5585763218,1768419719]
